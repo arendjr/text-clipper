f119768fd5cb8041a8f8346c056601c1ca6b138c
Arend van Beelen jr <arendjr@gmail.com>
2021-12-08 15:28:51 +0100

    Make benchmarks functional again

--- tests/perf/benchmark/benchmark.js
+++ tests/perf/benchmark/benchmark.js
@@ -1,7 +1,7 @@
-const Benchmark = require("benchmark");
+const { performance } = require("perf_hooks");
 
-const clip = require("../../../dist");
-const baseline = require("./baseline");
+const clip = require("../../../dist").default;
+const baseline = require("./baseline").default;
 const trimHtml = require("trim-html");
 const truncateHtml = require("truncate-html");
 const htmlTruncate = require("html-truncate");
@@ -21,31 +21,55 @@ for (var i = 0; i < 100000; i++) {
 const TEST_STRING = "Dit is een string die eigenlijk nog makkelijk past binnen de limiet.";
 const TEST_LIMIT = 100;
 
-const suite = new Benchmark.Suite();
+const MAX_RUN_TIME = 5000; // ms
 
-// add tests
-suite
-    .add("text-clipper", function () {
-        clip(TEST_STRING, TEST_LIMIT, { html: true });
-    })
-    .add("text-clipper-baseline", function () {
-        baseline(TEST_STRING, TEST_LIMIT, { html: true });
-    })
-    .add("trim-html", function () {
-        trimHtml(TEST_STRING, { limit: TEST_LIMIT });
-    })
-    .add("truncate-html", function () {
-        truncateHtml(TEST_STRING, TEST_LIMIT);
-    })
-    .add("html-truncate", function () {
-        htmlTruncate(TEST_STRING, TEST_LIMIT);
-    })
-    // add listeners
-    .on("cycle", function (event) {
-        console.log(String(event.target));
-    })
-    .on("complete", function () {
-        console.log(`Fastest is ${this.filter("fastest").map("name")}`);
-    })
-    // run async
-    .run({ async: true });
+const NUM_ITERATIONS_PER_MEASURE = 100;
+const MAX_NUM_MEASURES = 1000;
+
+const benchmarks = {
+    "text-clipper": (string, limit) => clip(string, limit, { html: true }),
+    "text-clipper-baseline": (string, limit) => baseline(string, limit, { html: true }),
+    "trim-html": (string, limit) => trimHtml(string, { limit }),
+    "truncate-html": truncateHtml,
+    "html-truncate": htmlTruncate
+};
+
+for (const [name, fn] of Object.entries(benchmarks)) {
+    console.log(`Benchmarking ${name}...`);
+
+    let fastest = Infinity;
+    let slowest = 0;
+
+    const begin = performance.now();
+    const measures = [];
+    
+    for (let i = 0; i < MAX_NUM_MEASURES; i++) {
+        const start = performance.now();
+        for (let j = 0; j < NUM_ITERATIONS_PER_MEASURE; j++) {
+            const clipped = fn(TEST_STRING, TEST_LIMIT);
+
+            // Just test to make sure the compiler doesn't eliminate everything.
+            if (clipped.length > TEST_STRING.length) {
+                process.exit("Clipped string is longer than input: " + clipped);
+            }
+        }
+        const finish = performance.now();
+
+        const time = finish - start;
+        fastest = Math.min(fastest, time);
+        slowest = Math.max(slowest, time);
+        measures.push(time);
+
+        if (finish - begin > MAX_RUN_TIME) {
+            break;
+        }
+    }
+
+    const average = measures.reduce((sum, measure) => sum + measure, 0) / measures.length;
+
+    console.log(`Average ops/s: ${toOps(average)} (fastest: ${toOps(fastest)}, slowest: ${toOps(slowest)})`);
+}
+
+function toOps(measure) {
+    return 1 / (measure / NUM_ITERATIONS_PER_MEASURE);
+}
