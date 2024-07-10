aeddd7e2b025fb919469ad72e34156f83d8509e1
Arend van Beelen jr <arend@arendjr.nl>
2024-07-10 20:55:57 +0200

    Deno first (#26)

--- tests/unit/examples.spec.ts
+++
@@ -1,8 +0,0 @@
-import clip from "../../src";
-
-test("examples: test examples from the README", () => {
-    expect(clip("foo", 3)).toBe("foo");
-    expect(clip("foo", 2)).toBe("f…");
-    expect(clip("foo bar", 5)).toBe("foo …");
-    expect(clip("foo\nbar", 5)).toBe("foo");
-});
